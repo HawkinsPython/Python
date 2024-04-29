364a6634070f18373101734b41cf37f849271d26
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-04-29 15:07:51 -0400

    sync

--- ex_04a_lists.py
+++ ex_04a_lists.py
@@ -2,7 +2,7 @@
 # print(thelist[0])
 # print(thelist[2])
 
-# foods = ["potatoes", "beef", "Broccoli", "lemons", "grapes"]
-# print(foods[1])
-# print(foods[-1])
-
+# mynums =[23, 7, 8, 10]
+# print(min(mynums))
+# print(max(mynums))
+# print(len(mynums))
