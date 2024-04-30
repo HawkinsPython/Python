4b70c8e71479ca324c68be6240cefb0df9b66a25
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-04-30 14:33:49 -0400

    sync

--- pe_pytnpnts_inpt.py
+++ pe_pytnpnts_inpt.py
@@ -1,20 +1,37 @@
-print("Hello!")
-name = input("please enter your name. ")
-print(f"Greetings, {name}.")
-print("This is a basic calculator that adds, subtracts, multiplies, and divides.")
-a = float(input("Please enter a number. "))
-b = float(input("Please enter a second number. "))
-c = a + b
-d = a - b
-e = a * b
-f = a / b
+# print("Hello!")
+# name = input("please enter your name. ")
+# print(f"Greetings, {name}.")
+# print("This is a basic calculator that adds, subtracts, multiplies, and divides.")
+# a = float(input("Please enter a number. "))
+# b = float(input("Please enter a second number. "))
+# c = a + b
+# d = a - b
+# e = a * b
+# f = a / b
 
-print(f"The sum is {c}.")
-print(f"The difference is {d}.")
-print(f"The product is {e}.")
-print(f"The quotient is {f:.4f}.")
-print("Now we are going to convert distance from km to m.")
-k = float(input("Input a number in kilometers. "))
-g = k * 1000
-print(f"In meters, that would be {g}m.")
-print("Have a good day!")
+# print(f"The sum is {c}.")
+# print(f"The difference is {d}.")
+# print(f"The product is {e}.")
+# print(f"The quotient is {f:.4f}.")
+# print("Now we are going to convert distance from km to m.")
+# k = float(input("Input a number in kilometers. "))
+# g = k * 1000
+# print(f"In meters, that would be {g}m.")
+# print("Have a good day!")
+
+
+# print("Hello!")
+# name = input("please enter your name. ")
+# print(f"Greetings, {name}.")
+# print("This is a basic calculator that adds, subtracts, multiplies, and divides.")
+# a = float(input("Please enter a number. "))
+# b = float(input("Please enter a second number. "))
+# print(f"The sum is {a + b}.")
+# print(f"The difference is {a-b}.")
+# print(f"The product is {a*b}.")
+# print(f"The quotient is {a/b:.4f}.")
+# print("Now we are going to convert distance from km to m.")
+# k = float(input("Input a number in kilometers. "))
+# g = k * 1000
+# print(f"In meters, that would be {g}m.")
+# print("Have a good day!")
