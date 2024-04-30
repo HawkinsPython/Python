c4e2c93b6d08770affa0a8b44777450582975b1e
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-04-30 11:10:13 -0400

    sync

---
+++ ex_04c_for_loops.py
@@ -0,0 +1,69 @@
+# colors = ["red", "orange", "yellow"]
+# for color in colors:
+#     print("Here is a color that I know:")
+#     print(color)
+#     print()
+# print("--------------------")
+# print("The for-loop has ended.")
+
+# colors = ["red", "orange","yellow"]
+# color = colors[0]
+# print("Here is a color that I know:")
+# print(color)
+# print()
+# color = colors[1]
+# print("Here is a color that I know:")
+# print(color)
+# print()
+# color = colors[2]
+# print("Here is a color that I know:")
+# print(color)
+# print()
+# print("------------------")
+# print("The for-loop has ended.")
+
+# names = ["Sam", "Lisa", "micah", "Dave"]
+# for name in names:
+#     print(f"Hello {name}.")
+# print("Welcome to the python course.")
+
+# ages = [26, 37, 55, 10, 5]
+# for age in ages:
+#     print(f"One of the people in my list is {age} years old.")
+#     print(f"In two years, that person will be {age + 2} years old.")
+#     print()
+
+# numbers = [21, 40, 32, 10, 8, 3]
+# for number in numbers:
+#     print(f"Half of {number} is {number/2}.")
+# print()
+
+# for num in range(1,5):
+#     print(num)
+
+# temps_in_F = [47, 100, 16, 82, 30, 68, 90, 25, 40]
+# for temp in temps_in_F:
+#     print(f"The temperature was {temp}")
+
+
+# temps_in_F = [47, 100, 16, 82, 30, 68, 90, 25, 40]
+# for temp in temps_in_F:
+#     print(f"The temperature was {temp}")
+#     if temp >90:
+#         print("that's hot!")
+
+# temps_in_F = [47, 100, 16, 82, 30, 68, 90, 25, 40]
+# for temp in temps_in_F:
+#     print(f"Looking at the temperatures....")
+#     print(f"One of the temperatures in the list is {temp}.")
+#     if temp >= 90:
+#         print("That's hot!")
+
+# x = input("Say a word: ")
+# if x.endswith("s"):
+#     print("That ends with an 's', so it mught be plural.")
+# print("That's all I have to say.")
+
+
+
+
