8ced871aa7380330c7dfeeac5565af238b5a379e
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-01 14:36:10 -0400

    sync

--- dy2.py
+++ dy2.py
@@ -1,23 +1,24 @@
-def convert_number(number):
-    binary = bin(number)[2:]
-    octal = oct(number)[2:]
-    hexadecimal = hex(number)[2:]
-    return binary, octal, hexadecimal
-first_integer = int(input("Give me an integer? "))
-second_integer = int(input("Give me another integer? "))
-first_binary, first_octal, first_hexadecimal = convert_number(first_integer)
-second_binary, second_octal, second_hexadecimal = convert_number(second_integer)
-print(f"Your first number in binary is {first_binary}. ")
-print(f"Your first number in octal is {first_octal}. ")
-print(f"Your first number in hexadecimal is {first_hexadecimal}. ")
-print()
-print(f"Your second number in binary is {second_binary}. ")
-print(f"Your second number in octal is {second_octal}. ")
-print(f"Your second number in hexadecimal is {second_hexadecimal}. ")
-print()
-sum_decimal = first_integer + second_integer
-sum_binary, sum_octal, sum_hexadecimal = convert_number(sum_decimal)
-print(f"The sum of your two numbers in decimal is {sum_decimal}. ")
-print(f"The sum of your two numbers in binary is {sum_binary}. ")
-print(f"The sum of your two numbers in octal is {sum_octal}. ")
-print(f"The sum of your two numbers in hexadecimal is {sum_hexadecimal}. ")
+# def convert_number(number):
+#     binary = bin(number)[2:]
+#     octal = oct(number)[2:]
+#     hexadecimal = hex(number)[2:]
+#     return binary, octal, hexadecimal
+# first_integer = int(input("Give me an integer? "))
+# second_integer = int(input("Give me another integer? "))
+# first_binary, first_octal, first_hexadecimal = convert_number(first_integer)
+# second_binary, second_octal, second_hexadecimal = convert_number(second_integer)
+# print(f"Your first number in binary is {first_binary}. ")
+# print(f"Your first number in octal is {first_octal}. ")
+# print(f"Your first number in hexadecimal is {first_hexadecimal}. ")
+# print()
+# print(f"Your second number in binary is {second_binary}. ")
+# print(f"Your second number in octal is {second_octal}. ")
+# print(f"Your second number in hexadecimal is {second_hexadecimal}. ")
+# print()
+# sum_decimal = first_integer + second_integer
+# sum_binary, sum_octal, sum_hexadecimal = convert_number(sum_decimal)
+# print(f"The sum of your two numbers in decimal is {sum_decimal}. ")
+# print(f"The sum of your two numbers in binary is {sum_binary}. ")
+# print(f"The sum of your two numbers in octal is {sum_octal}. ")
+# print(f"The sum of your two numbers in hexadecimal is {sum_hexadecimal}. ")
+
---
+++ p3.py
@@ -0,0 +1,18 @@
+# print ("Hello!")
+# name = input ("What is your name? ")
+# print (f"Greetings, {name}.")
+# a = float (input("Give me a number. "))
+# b = float (input("Give me a second number. "))
+# math = input ("Pick a mathematical operation. Your choices are 'add', 'subtract', 'multiply', 'divide'. ")
+# if math == "add":
+#     result = a + b
+#     print("The sum of these numbers are", result)
+# elif math == "subtract":
+#     result = a - b
+#     print("The difference of these numbers are", result)
+# elif math == "multiply":
+#     result = a * b
+#     print("The product of these numbers are", result)
+# elif math == "divide":
+#         result = a / b
+#         print("The quotient of these numbers are", result)
