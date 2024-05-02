2f09e1757bd377b7446f58b234c0ed69d0df4d16
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-02 10:10:50 -0400

    sync

---
+++ ex_6b_dictionaries.py
@@ -0,0 +1,46 @@
+# print("If you want to put everything ", end="")
+# print("on one line, then you can ", end="")
+# print("use this.")
+
+# mypersonaldata = {
+#     "firstname": "Bob",
+#     "lastname": "Smith",
+#     "age": 23
+# }
+# print(mypersonaldata["firstname"])
+
+# mypersonaldata = {
+#     "firstname": "Bob",
+#     "lastname": "Smith",
+#     "age": 23
+# }
+# print(f'Do you know {mypersonaldata["firstname"]}...?')
+
+# nameLocation = {
+#     "Sophia": "Greece",
+#     "Mario": "Italy",
+#     "Jaques": "France"
+# }
+# for name in nameLocation:
+#     print(name)
+#     print("is from")
+#     print(nameLocation[name])
+#     print()
+
+# import random
+# user = input("rock paper or scissors ")
+# beats = {
+#     "rock": "scissors",
+#     "Scissors": "paper",
+#     "paper": "rock"
+# }
+# choices = ["rock", "paper", "scissors"]
+# cpu = random.choice(choices)
+# print(f"cpu has selected: {cpu}")
+# if user == cpu:
+#     print("Tie")
+# elif beats[user] == cpu:
+#     print("You win!")
+# else:
+#     print("You lose!")
+
