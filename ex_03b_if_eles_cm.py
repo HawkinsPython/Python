bd3fd84a23f18d0f184fea8fe776741b648cc12b
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-02 10:52:56 -0400

    sync

---
+++ ex_03b_if_eles_cm.py
@@ -0,0 +1,106 @@
+# import random
+# randomNumber = random.randint(5, 8)
+# print(f"Hers's a random integer: {randomNumber}")
+
+# import random
+# randomNumber = random.randint(5, 8)
+# print(f"Here's a random integer: {randomNumber}")
+# if randomNumber == 7:
+#     print("I think some people say that number is lucky.")
+
+# import random
+# name = random.choice(["bob", "susan", "joe", "anna"])
+# print(f"Hey {name}.")
+# if name == "joe":
+#     print("Hello joe!")
+#     print("Your name rhymes with low")
+#     print("Have a good day.")
+
+# import random
+# age = random.randint(18, 24)
+# print(f"Pretend that you are {age} years old.")
+# if age < 21:
+#     print("You can't drink alcohol in the us yet.")
+# else:
+#     print("You are legally allowed to drink. Drink responsibly.")
+
+# import random
+# age = random.randint(18, 24)
+# print(f"pretend that you are {age} years old.")
+# if age < 21:
+#     print("You can't drink alcohol in the US yet.")
+# else:
+#     print("You are legally allowed to drink. Drink responsibly!")
+
+# thename = input("What is your name? ")
+# if thename == "george":
+#     print("Are you named after the first US President")
+# else:
+#     print("hello.")
+
+# name = input("What is your name? ")
+# print(f"The lowercase version of that is {name.lower()}.")
+# if name.lower() == "joe":
+#     print("Your name rhymes with low.")
+# else:
+#     print(f"Hey{name}.")
+
+# name = input("What is your name? ")
+# if name.lower() != "jay":
+#     print("Your name is not Jay.")
+# print("Greetings.")
+
+# age = int(input("How old are you?"))
+# ageNextyear = age + 1
+# print(ageNextyear)
+
+# birthyear = int(input("Type a year."))
+# if birthyear < 2000:
+#     print("You were born before 2000.")
+# elif birthyear == 2000:
+#     print("You were born in 2000.")
+# else:
+#     print("You were born after 2000.")
+
+# name = input("What is your name? (type it lowercase please.)")
+# print("Ok, let me look up that name...")
+# if name =="bob":
+#     print("That name use to be common, I think.")
+# elif name == "sue":
+#     print("Your name also refers to a legal action.")
+# elif name == "rob":
+#     print("Another abbreviation for robert, correct?")
+# elif name == "lacy":
+#     print("Does the origin of your name relate to lace?")
+# else:
+#     print("I don't know you.")
+# print("Done.")
+
+# x = int(input("Enter a number: ")) 
+# if x < 20: 
+#     print("A") 
+#     print("B") 
+# print("C") 
+
+# x = int(input("Type a number: "))
+# if 3 < x < 10:
+#     print("x is more than 3 and less than 10.")
+
+# firstcost = float(input("How much is the first thing you bought?"))
+# secondcost = float(input("How much is the second thing you bought?"))
+# total = firstcost + secondcost
+# discounted = total * 0.9
+# print(f"""The total cost would be {total}. However, we're doing a special today, so you get a 10% discount. That means you actually pay {discounted}""")
+
+# x = float(input("Type a non-whole number. "))
+# print(f"One more would be {x + 1}.")
+
+# color = input("What color is water?")
+# if color == "blue" or color == "transparent":
+#     print("yes I agree.")
+# else:
+#     print("I'm not sure about that.")
+
+
+
+    
--- ex_05b_while_loops.py
+++ ex_05b_while_loops.py
@@ -59,3 +59,56 @@
 #         break
 # print("Ok, stopping.")
 
+# count = 10
+# print(f"countis {count}")
+# count += 1
+# print(f"I changed count. Now, it's {count}.")
+# count -= 2
+# print(f"I changed it again. Now, it's {count}.")
+
+# count = 3
+# print(count)
+# count -= 1
+# print(count)
+# count -= 1
+# print(count)
+# count -= 1
+# print("Reached zero. Proof:")
+# print(count)
+
+# count = 3
+# while count > 0:
+#     print(count)
+#     count -= 1
+# print("Reached zero. Proof:")
+# print(count)
+
+# import time
+
+# print("start...")
+# time.sleep(1)
+# print("done.")
+
+# keepGoing = "yes"
+# while keepGoing == "yes":
+#     print("since the variable keepGoing is still 'yes', I am going to keep going.")
+#     keepGoing = input("Do you want me to keep going?")
+# print("Ok, you typed something other then 'yes', so I stopped.")
+
+# keepGoing = "yes"
+# while keepGoing == "yes" or keepGoing == "y":
+#     print("continuing")
+#     keepGoing = input("Do you want me to keep going?")
+# print("Ok, stopping.")
+
+# keepGoing = "yes"
+# while keepGoing in ["yes", "y"]:
+#     print("Continuing.")
+#     keepGoing = input("Do you want me to keep going? ")
+# print("Ok, stopping.")
+
+# keepGoing = "yes"
+# while keepGoing not in ["no", "no thanks"]:
+#     print("Continuing.")
+#     keepGoing = input("Do you want me to keep going? ")
+# print("Ok, stopping.")
