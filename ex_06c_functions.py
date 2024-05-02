ab4177d1a603c6a8d1e2d21dd8612de9a067851c
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-02 10:46:35 -0400

    sync

---
+++ ex_06c_functions.py
@@ -0,0 +1,16 @@
+# def sayWelcome():
+#     print("Welcome to the Python class")
+#     print("We hope you enjoy it.")
+#     print()
+
+# sayWelcome()
+# sayWelcome()
+
+# def sayWelcome():
+#     print("Welcome to the Python class")
+#     print("We hope you enjoy it.")
+#     print()
+
+# sayWelcome()
+# sayWelcome()
+# sayWelcome()
