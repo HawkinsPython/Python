2ca0d2d002591cf046f3b3481827468be2795099
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-02 10:18:54 -0400

    sync

---
+++ ex_06b_while_loops.py
@@ -0,0 +1,24 @@
+# count = 10
+# print(F"count is {count}.")
+# count += 1
+# print(f"I changed count. Now, it't {count}.")
+# count -= 2
+# print(f"I changed it again. Now, it's {count}.")
+
+
+# count = 3
+# print(count)
+# count -= 1
+# print(count)
+# count -= 1
+# print(count)
+# count -= 1
+# print("Reached zero. Proof:")
+# print(count)
+
+# count = 3
+# while count > 0:
+#     print(count)
+#     count -= 1
+# print("Reached zero. Proof:")
+# print(count)
