dc267e76e3856ab75b91bf053d6544bcd53b9816
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-03 13:39:42 -0400

    sync

--- python_4_for_loops_file_IO.py
+++ python_4_for_loops_file_IO.py
@@ -2,17 +2,17 @@ f = open("users.txt", "r")
 lines = f.read().splitlines()
 f.close()
 print(f"Data: {len(lines)} lines loaded.")
-output_filename = input("Filename for output file: ")
-print(f"Writing report to {output_filename}.")
-with open(output_filename, "w") as output_file:
-    count_old_enough = 0
+output = input("Filename for output file: ")
+print(f"Writing report to {output}.")
+with open(output, "w") as output:
+    count_old = 0
     for line in lines:
         name_str, age_str = line.split(",")
         age = int(age_str)
         if age >= 23:
-            count_old_enough += 1
-            output_file.write(f"{name_str} is {age} years old, and is old enough to ride a dinosaur.\n")
+            count_old += 1
+            output.write(f"{name_str} is {age} years old, and is old enough to ride a dinosaur.\n")
         else:
-            output_file.write(f"{name_str} is {age} years old, and is NOT old enough to ride a dinosaur.\n")
-    output_file.write(f"{count_old_enough} people are old enough to ride a dinosaur.\n")
+            output.write(f"{name_str} is {age} years old, and is NOT old enough to ride a dinosaur.\n")
+    output.write(f"{count_old} people are old enough to ride a dinosaur.\n")
 print("Report Completed.")
