0b98a3f178585d6ebc69555ab4e618e4e1822510
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-06 10:55:45 -0400

    sync

---
+++ tkinter.py
@@ -0,0 +1,17 @@
+# import tkinter
+# wn = tkinter.tk()
+# def down(e):
+#     print('down\n', e.char, '\n', e)
+# def up(e):
+#         print('up\n', e.char, '\n', e)
+# wn.bind('<keypress>', down)
+# wn.bind('<keyrelease>', up)
+# wn.mainloop()
+
+# import tkinter as tk
+# root = tk.Tk()
+# def motion(event):
+#     x, y = event.x, event.y
+#     print('{}, {}' .format(x, y))
+# root.bind('<motion>', motion)
+# root.mainloop()
