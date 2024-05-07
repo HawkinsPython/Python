f6910a5e8d2219f4b07b5bf75d53583e9f1e708e
HawkinsPython <168146963+HawkinsPython@users.noreply.github.com>
2024-05-07 10:30:27 -0400

    sync

--- scratch_paper.py
+++ scratch_paper.py
@@ -10,8 +10,17 @@
 # print(4 != 10)
 # print(20 % 80)
 
-num1 = int(input("Enter a number between 1 and 100 "))
-num2 = int(input("enter another number between 1 and 100 "))
-print("number 1 + number 2 =", num1+num2)
+# num1 = int(input("Enter a number between 1 and 100 "))
+# num2 = int(input("enter another number between 1 and 100 "))
+# print("number 1 + number 2 =", num1+num2)
 
-print("code will add the 2 variables together")
+# print("code will add the 2 variables together")
+
+print("""
+code          answer       explanation
+20**5         3200000      20 to the power of 5 
+2+6             8          addition of the two numbers
+8==8          True         checks if first number is equal to the second number
+4=10      syntax Error     Tries to store the value of 10 into a variable of 4, leads to a cannot assign to literal error.
+20%80           20         divides first number by the second number, but only gives you the remainder 
+""")
